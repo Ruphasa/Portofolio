1bdab368b7334ff2bf2df5e56b631dde2672c1e6
ruphasa <rfrizqifauzan@gmail.com>
2025-10-31 18:31:36 +0700

    Feat : try run build

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,9 @@ export default defineConfig({
   // If you're deploying this repo (rizqi-fauzan-persona-showcase), set it to "/rizqi-fauzan-persona-showcase/".
   base: "/Portofolio/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
