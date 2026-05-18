b35e1467e3ca7b72e2d1d2cc920239a9e132fb5c
ruphasa <rfrizqifauzan@gmail.com>
2026-05-18 14:06:42 +0700

    Fix Pages deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ruphasa.github.io',
-  base: '/rizqi-fauzan-persona-showcase',
+  base: '/Portofolio',
   integrations: [react()],
 });
